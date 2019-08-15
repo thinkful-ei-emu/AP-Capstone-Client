c10fdac3f323573ed8b3ee34a28138b8ae8e0cb9
Arpan Patel <adp8ke@gmail.com>
2019-08-15 08:18:16 -0400

    all fxnalility working to deployed server and db

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://blooming-reef-25668.herokuapp.com/api',
     TOKEN_KEY: 'dog-parks-client-auth-token',
 }
