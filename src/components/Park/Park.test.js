aa1a4aad042bd97962bd60bcc6eee94e34a9d8c4
Arpan Patel <adp8ke@gmail.com>
2019-08-15 14:39:36 -0400

    added smoke tests for ParkList and Park components

--- src/components/Park/Park.test.js
+++ src/components/Park/Park.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ParkList from './ParkList';
+import Park from './Park';
 
 import ReactDOM from 'react-dom'
 
@@ -11,29 +11,31 @@ import {MemoryRouter} from 'react-router'
 
 it('renders without crashing', () => {
 
-    let parks = [
+    let match ={
+        url: '/api/parks/1',
+        params: {
+            id: 1
+        }
+    }
+
+    let park = 
         {
             id: 1,
             park_name: "Bayville Farms Park",
             park_city: "Virginia Beach",
             park_address: "4132 First Court Road",
             park_hours: "Please Consult for Hours: https://www.vbgov.com/government/departments/parks-recreation/parks-trails/city-parks/Documents/park-closing-times.pdf"
-          },
-          {
-            id: 4,
-            park_name: "Maple Avenue Dog Park",
-            park_city: "Norfolk",
-            park_address: "176 Maple Ave",
-            park_hours: "7 AM - 6 PM"
-            
-          },
-          {
-            id: 12,
-            park_name: "Chesapeake City Park",
-            park_city: "Chesapeake",
-            park_address: "900 City Park Drive",
-            park_hours: "7 AM - 8 PM"
           }
+
+    let reviews = [
+        {
+            id: 1,
+            rating: 2,
+            text: 'First test review!',
+            park_id: 1,
+            user_id: 1,
+            date_created: new Date('2029-01-22T16:28:32.615Z').toLocaleDateString(),
+          },
     ]
     
 
@@ -41,10 +43,11 @@ it('renders without crashing', () => {
     ReactDOM.render(
         <ParksContext.Provider
         value={{
-            parks: parks
+            park,
+            reviews,
         }}>
             <MemoryRouter>
-                <ParkList/>
+                <Park match={match}/>
             </MemoryRouter>
         </ParksContext.Provider>, div);
     ReactDOM.unmountComponentAtNode(div);
