330886d67c44a20ba006da100df169bc842a3615
Arpan Patel <adp8ke@gmail.com>
2019-10-09 14:27:29 -0400

    forgot to change config back

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-//   API_ENDPOINT: 'https://blooming-reef-25668.herokuapp.com/api',
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://blooming-reef-25668.herokuapp.com/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'dog-parks-client-auth-token',
 };
